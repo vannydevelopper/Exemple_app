4534fc084375c18f5fcc007721b71b6233774a79
vanny <vdevelopper@gmail.com>
2022-08-24 18:22:18 +0200

    ajout du nouveau disign

--- src/screens/homes/HomeScreen.jsx
+++ src/screens/homes/HomeScreen.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { ScrollView,StyleSheet,BackHandler,Animated, TouchableOpacity,TouchableWithoutFeedback, StatusBar, Text, View } from "react-native";
-import { Entypo, FontAwesome5, AntDesign, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
+import { ScrollView, StyleSheet, BackHandler, Animated, TouchableOpacity, TouchableWithoutFeedback, StatusBar, Text, View } from "react-native";
+import { Entypo, FontAwesome5, AntDesign, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { Host, Portal } from 'react-native-portalize';
 import { Modalize } from 'react-native-modalize';
 import DateTimePicker from '@react-native-community/datetimepicker'
 
-const ExempleModal = ({onClose}) => {
+const ExempleModal = ({ onClose }) => {
        const [scale] = useState(new Animated.Value(1.1))
-       useEffect(()=>{
+       useEffect(() => {
               const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
                      onClose()
                      return true
-              }) 
+              })
               Animated.spring(scale, {
                      toValue: 1,
                      useNativeDriver: true
@@ -19,13 +19,13 @@ const ExempleModal = ({onClose}) => {
               return () => {
                      backHandler.remove()
               }
-       },[])
-       return(
+       }, [])
+       return (
               <Portal>
-                      <TouchableWithoutFeedback onPress={onClose}>
+                     <TouchableWithoutFeedback onPress={onClose}>
                             <View style={styles.modalContainer}>
                                    <TouchableWithoutFeedback>
-                                          <Animated.View style={{...styles.modalContent, transform: [{ scale }]}}>
+                                          <Animated.View style={{ ...styles.modalContent, transform: [{ scale }] }}>
                                                  <View style={{ borderBottomWidth: 0, paddingVertical: 10, paddingHorizontal: 20, backgroundColor: '#F58424' }}>
                                                         <Text style={{ fontWeight: 'bold', color: '#fff', opacity: 0.8, textAlign: 'center' }}>
                                                                Partenaires
@@ -37,25 +37,25 @@ const ExempleModal = ({onClose}) => {
                                                                       <View style={styles.modalItem}>
                                                                              <MaterialCommunityIcons name="radiobox-marked" size={24} color="#007bff" />
                                                                              <Text numberOfLines={1} style={styles.modalText}>Les outils</Text>
-                                                                      </View>    
+                                                                      </View>
                                                                </TouchableWithoutFeedback>
                                                         </View>
                                                  </ScrollView>
-                                          </Animated.View>  
+                                          </Animated.View>
                                    </TouchableWithoutFeedback>
                             </View>
-                      </TouchableWithoutFeedback>
+                     </TouchableWithoutFeedback>
               </Portal>
        )
 }
 
-const DeclareModal = ({onClose}) => {
+const DeclareModal = ({ onClose }) => {
        const [scale] = useState(new Animated.Value(1.1))
-       useEffect(()=>{
+       useEffect(() => {
               const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
                      onClose()
                      return true
-              }) 
+              })
               Animated.spring(scale, {
                      toValue: 1,
                      useNativeDriver: true
@@ -63,13 +63,13 @@ const DeclareModal = ({onClose}) => {
               return () => {
                      backHandler.remove()
               }
-       },[])
-       return(
+       }, [])
+       return (
               <Portal>
                      <TouchableWithoutFeedback onPress={onClose}>
                             <View style={styles.modalContainer}>
                                    <TouchableWithoutFeedback>
-                                          <Animated.View style={{...styles.modalContent, transform: [{ scale }]}}>
+                                          <Animated.View style={{ ...styles.modalContent, transform: [{ scale }] }}>
                                                  <View style={{ borderBottomWidth: 0, paddingVertical: 10, paddingHorizontal: 20, backgroundColor: '#F58424' }}>
                                                         <Text style={{ fontWeight: 'bold', color: '#fff', opacity: 0.8, textAlign: 'center' }}>
                                                                Partenaires
@@ -81,21 +81,21 @@ const DeclareModal = ({onClose}) => {
                                                                       <View style={styles.modalItem}>
                                                                              <MaterialCommunityIcons name="radiobox-marked" size={24} color="#007bff" />
                                                                              <Text numberOfLines={1} style={styles.modalText}>Test</Text>
-                                                                      </View>    
+                                                                      </View>
                                                                </TouchableWithoutFeedback>
                                                         </View>
                                                  </ScrollView>
-                                          </Animated.View>  
+                                          </Animated.View>
                                    </TouchableWithoutFeedback>
                             </View>
-                      </TouchableWithoutFeedback>
+                     </TouchableWithoutFeedback>
               </Portal>
        )
 }
 
 
 
-export default function HomeScreen(){
+export default function HomeScreen() {
        const [showPartenaires, setshowPartenaires] = useState(false)
        const [showDeclarations, setshowDeclarations] = useState(false)
        // const declareRef = useRef(null)
@@ -117,12 +117,13 @@ export default function HomeScreen(){
        const displayDatepicker = () => {
               showMode('date');
        };
-      
 
-       return(
+
+       return (
               <>
+              <ScrollView>
                      <View style={styles.container}>
-                            <StatusBar backgroundColor="#61dafb"/>
+                            <StatusBar backgroundColor="#61dafb" />
                             <View style={styles.header}>
                                    <View style={styles.icone}>
                                           <FontAwesome5 name="user" size={30} color="black" />
@@ -135,20 +136,20 @@ export default function HomeScreen(){
                                    </View>
                             </View>
                             <View>
-                                   <TouchableOpacity style={styles.modelCard} onPress={()=>setshowPartenaires(true)}>
+                                   <TouchableOpacity style={styles.modelCard} onPress={() => setshowPartenaires(true)}>
                                           <Text style={styles.inputText}>
                                                  Selectionner
                                           </Text>
                                           <AntDesign name="caretdown" size={16} color="#777" />
-                                   </TouchableOpacity>  
+                                   </TouchableOpacity>
                             </View>
                             <View>
-                                   <TouchableOpacity style={styles.modelCard} onPress={()=>setshowDeclarations(true)}>
+                                   <TouchableOpacity style={styles.modelCard} onPress={() => setshowDeclarations(true)}>
                                           <Text style={styles.inputText}>
                                                  Selectionner
                                           </Text>
                                           <AntDesign name="caretdown" size={16} color="#777" />
-                                   </TouchableOpacity>  
+                                   </TouchableOpacity>
                             </View>
                             <TouchableOpacity style={styles.datePickerButton} onPress={displayDatepicker}>
                                    <View style={styles.iconDebutName}>
@@ -162,7 +163,7 @@ export default function HomeScreen(){
                                    </View>
                             </TouchableOpacity>
 
-                            { isDisplayDate && <DateTimePicker
+                            {isDisplayDate && <DateTimePicker
                                    testID="dateTimePicker"
                                    value={mydate}
                                    mode={displaymode}
@@ -170,68 +171,82 @@ export default function HomeScreen(){
                                    display="default"
                                    onChange={changeSelectedDate}
                             />}
+                            <View style={styles.card}>
+                                   <View style={styles.cardPrincipale}>
+                                                 <Text>Candidat a la vaccination</Text>
+                                   </View>
+                                   <View style={styles.cardPrincipale2}>
+                                          <View style={styles.cardReponse}>
+                                                 <Text style={styles.labelName}>Nom</Text>
+                                                 <Text style={styles.labelValue}>AZESENGA </Text>
+                                          </View>
+                                          
+                                   </View>
+                            </View>
+
                      </View>
-                    {showPartenaires && <ExempleModal onClose={() => setshowPartenaires(false)}/>}
-                    {/* <Portal>
+                     </ScrollView>
+                     {showPartenaires && <ExempleModal onClose={() => setshowPartenaires(false)} />}
+                     {/* <Portal>
                             <Modalize ref={declareRef} adjustToContentHeight handleStyle={{ display: 'none' }} modalStyle={{ borderTopRightRadius: 20, borderTopLeftRadius: 20 }}>
                                    <DeclareModalize/>
                             </Modalize>
                     </Portal> */}
-                    {showDeclarations && <DeclareModal onClose={() => setshowDeclarations(false)}/>}
+                     {showDeclarations && <DeclareModal onClose={() => setshowDeclarations(false)} />}
               </>
        )
 }
 
 const styles = StyleSheet.create({
-       container:{
+       container: {
               flex: 1,
               // backgroundColor:"red"
-              
+
        },
-       title:{
+       title: {
               fontSize: 20,
-              color :"#777",
-              fontWeight:"bold"
+              color: "#777",
+              fontWeight: "bold"
        },
-       icone:{
+       icone: {
               width: 50,
               height: 50,
-              backgroundColor:"#ddd",
+              backgroundColor: "#ddd",
               borderRadius: 50,
-              justifyContent:"center",
-              alignItems:"center"
+              justifyContent: "center",
+              alignItems: "center"
        },
-       header:{
-              flexDirection:"row",
-              justifyContent:"space-between",
-              alignItems:"center",
+       header: {
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
               marginHorizontal: 10,
               marginTop: 10
        },
-       cardTitle:{
+       cardTitle: {
               width: "40%",
               height: 30,
-              backgroundColor:"#ddd",
-              justifyContent:"center",
-              alignItems:"center",
+              backgroundColor: "#ddd",
+              justifyContent: "center",
+              alignItems: "center",
               borderRadius: 20
        },
-       inputText:{
+       inputText: {
               fontSize: 17,
-              color:"#777"
+              color: "#777"
        },
-       modelCard:{
-              justifyContent:"space-between",
-              flexDirection:"row",
-              marginHorizontal:10,
+       modelCard: {
+              justifyContent: "space-between",
+              flexDirection: "row",
+              marginHorizontal: 10,
               marginTop: 10,
-              backgroundColor:"#fff",
+              backgroundColor: "#fff",
               padding: 9,
               borderRadius: 10,
-              borderWidth:1,
-              borderColor:"#ddd"
+              borderWidth: 1,
+              borderColor: "#ddd"
        },
-       modalContainer:{
+       modalContainer: {
               position: 'absolute',
               zIndex: 1,
               width: '100%',
@@ -240,7 +255,7 @@ const styles = StyleSheet.create({
               justifyContent: 'center',
               alignItems: 'center'
        },
-       modalContent:{
+       modalContent: {
               width: '90%',
               maxWidth: 400,
               backgroundColor: '#fff',
@@ -248,7 +263,7 @@ const styles = StyleSheet.create({
               overflow: 'hidden',
               maxHeight: '90%'
        },
-       modalContent2:{
+       modalContent2: {
               paddingBottom: 20
        },
        modalItem: {
@@ -259,7 +274,7 @@ const styles = StyleSheet.create({
               alignItems: 'center',
               alignContent: 'center'
        },
-       modalItem2:{
+       modalItem2: {
               paddingVertical: 10,
               paddingHorizontal: 15,
               marginTop: 5,
@@ -267,7 +282,7 @@ const styles = StyleSheet.create({
               alignItems: 'center',
               alignContent: 'center'
        },
-       modalList2:{},
+       modalList2: {},
        modalText: {
               fontWeight: 'bold',
               marginLeft: 10
@@ -290,11 +305,54 @@ const styles = StyleSheet.create({
               padding: 9,
               justifyContent: "space-between",
               marginTop: 10,
-              marginHorizontal:10
+              marginHorizontal: 10
        },
        rightDateText: {
               fontWeight: 'bold',
               padding: 2,
               borderRadius: 5
-    },
+       },
+       cardPrincipale: {
+              // marginHorizontal: 10,
+              padding: 10,
+              backgroundColor: "#ddd",
+              // borderRadius: 10,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              elevation: 8
+
+       },
+       cardPrincipale2: {
+              // marginHorizontal: 10,
+              padding: 10,
+              backgroundColor: "#fff",
+              elevation: 8,
+       },
+       card:{
+              borderWidth:1,
+              marginHorizontal: 10,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+       },
+       labelName:{
+              fontSize:15,
+              marginLeft: 5
+       },
+       cardReponse:{
+              flexDirection:"row"
+       },
+       labelValue:{
+              fonntSize:17,
+              color:"blue",
+              fontWeight:"bold",
+              marginLeft: 10
+       }
+       // ligneSeparator:{
+       //        borderTopWidth: 1,
+       //        marginTop: 10,
+       //        borderTopColor: "#F58424"
+       // },
+       // cardHaut:{
+       //        backgroundColor:"#777"
+       // }
 })
